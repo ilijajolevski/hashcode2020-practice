36c050b6df7f0eb534493c75345f84a87c0774ef
Ilija Jolevski <ilija.jolevski@gmail.com>
2020-02-19 22:12:04 +0100

    added slices

--- main.go
+++ main.go
@@ -7,12 +7,19 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	var lineNumber int
+	var maxPizzaSlices = 0
+	var PizzaTypes = 0
 
 	fptr := flag.String("fpath", "./a_example.in", "file path to read from")
+	//fptr := flag.String("fpath", "./b_small.in", "file path to read from")
+	//fptr := flag.String("fpath", "./c_medium.in", "file path to read from")
+	//fptr := flag.String("fpath", "./d_quite_big.in", "file path to read from")
+	//fptr := flag.String("fpath", "./e_also_big.in", "file path to read from")
 
 	flag.Parse()
 
@@ -28,7 +35,30 @@ func main() {
 	s := bufio.NewScanner(f)
 	//s.Scan()
 	for s.Scan() {
-		fmt.Println(s.Text())
+
+		if lineNumber == 0 {
+			fmt.Println(s.Text())
+			words := strings.Fields(s.Text())
+
+			maxPizzaSlices, err = strconv.Atoi(words[0])
+			if err != nil {
+				// handle error
+			}
+			PizzaTypes, err = strconv.Atoi(words[1])
+			if err != nil {
+				// handle error
+			}
+
+			fmt.Println("Max Pizza Slices: " + strconv.Itoa(maxPizzaSlices))
+			fmt.Println("Pizza Types: " + strconv.Itoa(PizzaTypes))
+		}
+
+		if lineNumber == 1 {
+			//fmt.Println(s.Text())
+			//words := strings.Fields(s.Text())
+			sliceData := strings.Split(s.Text(), " ")
+			fmt.Println(sliceData)
+		}
 		//read line by line
 
 		lineNumber++
